ec8b0a619cfd063d1a541a790578c1d9593bae2c
shivasura <kaleljcolon@gmail.com>
2024-04-26 00:07:59 -0600

    Co-authored-by: Jack Rigan <JackXN@users.noreply.github.com>

--- js/site.js
+++ js/site.js
@@ -1,1 +1,28 @@
 
+
+
+
+
+const BASE_API_URL = 'https://opentdb.com/api.php?amount=10&encode=url3986';
+
+const questions = [];
+const score = 0;
+
+
+
+function getQuestions() {
+    fetch(BASE_API_URL).then(response => response.json()).then(data => {
+        questions = data.results;
+        console.log(questions)
+
+    })
+}
+
+
+
+getQuestions()
+
+
+
+
+
